b322ce64f82b38fe698b8e4c1e7ff4e18f132c4a
selimcanozgur <webisdeveloper@gmail.com>
2024-10-22 14:52:00 +0300

    create table added

--- server/database/connect.go
+++ server/database/connect.go
@@ -35,4 +35,36 @@ func Connect() {
 		log.Fatal("Veritabanına bağlanılamadı, sunucuya erişilemiyor:", err)
 	}
 
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+	createBookTable()
+
+}
+
+func createBookTable () {
+	bookTable := `
+	CREATE TABLE IF NOT EXISTS books (
+	 id INT AUTO_INCREMENT PRIMARY KEY,
+     title VARCHAR(100) NOT NULL,
+     author VARCHAR(100) NOT NULL,
+     publishing_house VARCHAR(100) NOT NULL,
+     publishing_year YEAR NOT NULL,
+     price DECIMAL(8,2) NOT NULL,
+     pages INT NOT NULL,
+     summary LONGTEXT NOT NULL,
+     lang VARCHAR(20) NOT NULL,
+     cover_type ENUM('Ciltli', 'İnce Kapak') NOT NULL,
+     image_url VARCHAR(255),
+     isbn VARCHAR(13) UNIQUE,
+     stock INT NOT NULL DEFAULT 0,
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	)`
+
+	_, err := DB.Exec(bookTable)
+
+	if err != nil {
+		log.Fatal("Tablo oluşturulamadı.", err)
+	}
 }
